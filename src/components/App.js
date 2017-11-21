d5ee1d396e8fe84d33fecc2e1db51e9e94e19499
Prima Aulia Gusta <primaulia@gmail.com>
2017-11-21 22:24:15 +0800

    latest update with product update and search

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,55 @@ import './App.css'
 import Nav from './Nav'
 import Product from './Product'
 
+// from the db
+const products = [
+  {
+    title: 'Product Hunt Project X',
+    subtitle: 'Something different from the Product Hunt team',
+    subCount: 2716
+  },
+  {
+    title: 'MeetPro',
+    subtitle: 'Meet with investors, no warm introduction needed ✨',
+    subCount: 85
+  },
+  {
+    title: 'Plutio',
+    subtitle: '🚨 One place for everything to run your freelance business 👌',
+    subCount: 524
+  },
+  {
+    title: 'Tesla Powerbank',
+    subtitle: 'A Tesla supercharger for your phone!',
+    subCount: 816
+  },
+  {
+    title: 'Briefcase by AppSumo',
+    subtitle: 'Netflix for software',
+    subCount: 581
+  },
+  {
+    title: 'Product Reviews',
+    subtitle: 'Concise and unbiased software product reviews',
+    subCount: 375
+  },
+  {
+    title: 'Ello 3.0',
+    subtitle: 'A social network for artists',
+    subCount: 268
+  },
+  {
+    title: 'Orbitkey Ring',
+    subtitle: 'The keyring, reinvented',
+    subCount: 188
+  },
+  {
+    title: 'Who is mining?',
+    subtitle: 'A simple tool to see which sites are mining cryptocurrency',
+    subCount: 148
+  }
+]
+
 class App extends Component {
   constructor (props) {
     super()
@@ -11,12 +60,13 @@ class App extends Component {
     // this is where i init the state
     this.state = {
       counter: 12,
-      keyword: ''
+      keyword: '',
+      products: products,
+      newProduct: {}
     }
   }
 
   clickButton (e) {
-    console.log('button is clicked')
     this.setState({
       counter: this.state.counter + 1
     })
@@ -25,7 +75,31 @@ class App extends Component {
   handleKeyup (e) {
     var typedValue = e.target.value
     this.setState({
-      keyword: typedValue
+      keyword: typedValue,
+      newProduct: {
+        title: typedValue,
+        subtitle: 'Test',
+        subCount: 0
+      }
+    })
+  }
+
+  addProduct (e) {
+    this.setState({
+      products: [
+        ...this.state.products,
+        this.state.newProduct
+      ],
+      newProduct: {}
+    })
+  }
+
+  handleSearch (e) {
+    let filteredProducts = products.filter(product => {
+      return product.title.toLowerCase().includes(e.target.value)
+    })
+    this.setState({
+      products: filteredProducts
     })
   }
 
@@ -36,56 +110,7 @@ class App extends Component {
       useremail: 'shumin@gmail.com'
     }
 
-    // from the db
-    const products = [
-      {
-        title: 'Product Hunt Project X',
-        subtitle: 'Something different from the Product Hunt team',
-        subCount: 2716
-      },
-      {
-        title: 'MeetPro',
-        subtitle: 'Meet with investors, no warm introduction needed ✨',
-        subCount: 85
-      },
-      {
-        title: 'Plutio',
-        subtitle: '🚨 One place for everything to run your freelance business 👌',
-        subCount: 524
-      },
-      {
-        title: 'Tesla Powerbank',
-        subtitle: 'A Tesla supercharger for your phone!',
-        subCount: 816
-      },
-      {
-        title: 'Briefcase by AppSumo',
-        subtitle: 'Netflix for software',
-        subCount: 581
-      },
-      {
-        title: 'Product Reviews',
-        subtitle: 'Concise and unbiased software product reviews',
-        subCount: 375
-      },
-      {
-        title: 'Ello 3.0',
-        subtitle: 'A social network for artists',
-        subCount: 268
-      },
-      {
-        title: 'Orbitkey Ring',
-        subtitle: 'The keyring, reinvented',
-        subCount: 188
-      },
-      {
-        title: 'Who is mining?',
-        subtitle: 'A simple tool to see which sites are mining cryptocurrency',
-        subCount: 148
-      }
-    ]
-
-    const allProducts = products.map((product, index) => {
+    const allProducts = this.state.products.map((product, index) => {
       return <Product key={index} productObj={product} />
     })
 
@@ -96,15 +121,21 @@ class App extends Component {
         />
         <div className='container'>
           <div className='row'>
-            <h1>New Product: {this.state.keyword}</h1>
-          </div>
-          <div className='row'>
             <div className='input-field col s6'>
               <input id='new-product' type='text'
-                className=''
-                onKeyUp={(e) => this.handleKeyup(e)} />
+                onKeyUp={(e) => this.handleKeyup(e)}
+              />
               <label className='active' htmlFor='new-product'>
                 New Product
+              </label>
+              <button className='btn' onClick={(e) => this.addProduct(e)}>+</button>
+            </div>
+            <div className='input-field col s6'>
+              <input id='search-product' type='text'
+                onKeyUp={(e) => this.handleSearch(e)}
+              />
+              <label className='active' htmlFor='search-product'>
+                Search
               </label>
             </div>
           </div>
