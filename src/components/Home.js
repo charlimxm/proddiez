9e840ceebffc8f759c133e0ed589d7f4bdd2b225
Prima Aulia Gusta <primAulia@gmail.com>
2017-11-27 13:54:40 +0800

    latest

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,9 @@ class Home extends Component {
     super()
 
     this.state = {
-      users: [] // init with empty array, cos we dont have any data yet
+      users: [], // init with empty array, cos we dont have any data yet
+      currentuser: {},
+      allPosts: []
     }
   }
 
@@ -21,12 +23,12 @@ class Home extends Component {
       useremail: 'shumin@gmail.com'
     }
 
-    const allUsers = this.state.users.map(user => {
+    const allPosts = this.state.allPosts.map(post => {
       return (
-        <div key={user.id} className="col s12 m4">
+        <div className="col s12 m4">
           <div className="card blue-grey darken-1">
             <div className="card-content white-text">
-              <span className="card-title">{ user.name }</span>
+              <span className="card-title">{ post.title }</span>
             </div>
           </div>
         </div>
@@ -41,7 +43,11 @@ class Home extends Component {
 
         <div className="container">
           <div className="row">
-            { allUsers }
+            <h2>All posts by {this.state.currentuser.name}</h2>
+
+            {
+              allPosts
+            }
           </div>
         </div>
       </div>
@@ -50,16 +56,32 @@ class Home extends Component {
 
 
   componentDidMount() {
-    // fetch data here
-    fetch('http://localhost:4000/users.json')
+    // fetch current user
+    fetch('http://localhost:4000/users/1.json')
     .then(response => response.json())
-    .then(usersFromDb => {
+    .then(currentuserFromDb => {
       // update the state if we got the data
 
       console.log('got data now')
-      console.log(usersFromDb)
+      console.log(currentuserFromDb)
       this.setState({
-        users: usersFromDb
+        currentuser: currentuserFromDb
+      })
+
+      this.getPostByUserId(currentuserFromDb.id)
+
+    })
+  }
+
+  getPostByUserId(user_id) {
+    fetch(`http://localhost:4000/users/${user_id}/posts`)
+    .then(response => response.json())
+    .then(postsFromUsers => {
+      console.log('received the posts now')
+      console.log(postsFromUsers)
+
+      this.setState({
+        allPosts: postsFromUsers
       })
     })
   }
