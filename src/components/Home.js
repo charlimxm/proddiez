3d11c0d6ab167aad7d33f4a099c44282689b66a3
Prima Aulia Gusta <primAulia@gmail.com>
2017-11-28 14:14:42 +0800

    latest with graphql

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import Nav from './Nav'
+
+const QUERY = gql`
+  # 2
+  query Query {
+    allProducts {
+      title
+      subtitle
+    }
+    allCategories {
+      name
+    }
+  }
+`
 
 class Home extends Component {
 
   // init the state here
-  constructor() {
+  constructor(props) {
     super()
+
+    console.log('props at constructor', props.data)
 
     this.state = {
       users: [], // init with empty array, cos we dont have any data yet
@@ -23,12 +40,31 @@ class Home extends Component {
       useremail: 'shumin@gmail.com'
     }
 
-    const allPosts = this.state.allPosts.map(post => {
+    console.log('props at render', this.props.data)
+
+    if (this.props.data.loading) {
+      return (
+        <div>
+          <Nav
+            navProp={navProp}
+          />
+
+          <div className="container">
+            <div className="row">
+              <h2>Loading Data...</h2>
+            </div>
+          </div>
+        </div>
+      )
+    }
+    
+    const allPosts = this.props.data.allProducts.map(post => {
       return (
         <div className="col s12 m4">
           <div className="card blue-grey darken-1">
             <div className="card-content white-text">
               <span className="card-title">{ post.title }</span>
+              <p>{ post.subtitle }</p>
             </div>
           </div>
         </div>
@@ -43,7 +79,7 @@ class Home extends Component {
 
         <div className="container">
           <div className="row">
-            <h2>All posts by {this.state.currentuser.name}</h2>
+            <h2>All Products</h2>
 
             {
               allPosts
@@ -52,25 +88,26 @@ class Home extends Component {
         </div>
       </div>
     )
+
   }
 
 
   componentDidMount() {
     // fetch current user
-    fetch('http://localhost:4000/users/1.json')
-    .then(response => response.json())
-    .then(currentuserFromDb => {
-      // update the state if we got the data
-
-      console.log('got data now')
-      console.log(currentuserFromDb)
-      this.setState({
-        currentuser: currentuserFromDb
-      })
-
-      this.getPostByUserId(currentuserFromDb.id)
-
-    })
+    // fetch('http://localhost:4000/users/1.json')
+    // .then(response => response.json())
+    // .then(currentuserFromDb => {
+    //   // update the state if we got the data
+    //
+    //   console.log('got data now')
+    //   console.log(currentuserFromDb)
+    //   this.setState({
+    //     currentuser: currentuserFromDb
+    //   })
+    //
+    //   this.getPostByUserId(currentuserFromDb.id)
+    //
+    // })
   }
 
   getPostByUserId(user_id) {
@@ -87,4 +124,4 @@ class Home extends Component {
   }
 }
 
-export default Home
+export default graphql(QUERY)(Home)
--- src/index.js
+++ src/index.js
@@ -5,5 +5,22 @@ import './index.css'
 import Home from './components/Home'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Home />, document.getElementById('root'))
+// all packages that i need to connect to my express server + graphql
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = new HttpLink({ uri: 'http://localhost:3000/graphql' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Home />
+  </ApolloProvider>
+, document.getElementById('root'))
 registerServiceWorker()
